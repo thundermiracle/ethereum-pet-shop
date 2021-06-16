998341b19afa2053b05358194ff0679b038e74c9
thundermiracle <cissoliu@gmail.com>
2021-06-16 22:23:30 +0900

    finish tutorial & finish readme

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # pet shop's tutorial
 
 for study ethereum & solidity
+
+## How to make it work
+
+1. Install Ganache & make a `QuickStart` workspace
+    - Download from [here](https://www.trufflesuite.com/ganache) and install
+    - Make a quickstart workspace
+1. Install Metamask
+    https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn
+1. Connect Metamask to Ganache
+    - Create a new network
+      - Network Name: localhost:7545
+      - RPC URL: http://127.0.0.1:7545
+      - Chain ID: 1337
+      - Save
+    - Select `localhost:7545` and all accounts will be listed
+1. Install npm packages
+    ```shell
+    yarn
+    ```
+1. Deploy smart contract
+    ```shell
+    yarn deploy
+    ```
+1. Start web server & check http://localhost:3000
+    ```shell
+    yarn dev
+    ```
--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,130 @@
 App = {
   web3Provider: null,
-  contracts: {},
+  contracts: {
+    Adoption: null,
+  },
 
-  init: async function() {
+  init: async function () {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    $.getJSON("../pets.json", function (data) {
+      var petsRow = $("#petsRow");
+      var petTemplate = $("#petTemplate");
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+      for (i = 0; i < data.length; i++) {
+        petTemplate.find(".panel-title").text(data[i].name);
+        petTemplate.find("img").attr("src", data[i].picture);
+        petTemplate.find(".pet-breed").text(data[i].breed);
+        petTemplate.find(".pet-age").text(data[i].age);
+        petTemplate.find(".pet-location").text(data[i].location);
+        petTemplate.find(".btn-adopt").attr("data-id", data[i].id);
 
         petsRow.append(petTemplate.html());
       }
     });
 
-    return await App.initWeb3();
+    await App.initWeb3();
+
+    App.initContract();
+    App.bindEvents();
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+  initWeb3: async function () {
+    if (window.ethereum) {
+      // Modern dapp browser
+      App.web3Provider = window.ethereum;
+      try {
+        // request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    } else if (window.web3) {
+      // Legacy dapp browser
+      App.web3Provider = window.web3.currentProvider;
+    } else {
+      // Fallback to localhost ganache
+      App.web3Provider = new Web3.provider.HttpProvider(
+        "http://localhost:7545"
+      );
+    }
 
-    return App.initContract();
+    // initialize global web3
+    web3 = new Web3(App.web3Provider);
   },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+  initContract: function () {
+    $.getJSON("Adoption.json", function (data) {
+      const AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
 
-    return App.bindEvents();
+      // set the provider
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // use our contracts to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  bindEvents: function () {
+    $(document).on("click", ".btn-adopt", App.handleAdopt);
   },
 
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
+  markAdopted: function () {
+    // define adoptionInstance to prevent multiple requests
+    let adoptionInstance;
+
+    App.contracts.Adoption.deployed()
+      .then((instance) => {
+        adoptionInstance = instance;
+        return adoptionInstance.getAdopters.call();
+      })
+      .then((adopters) => {
+        for (i = 0; i < adopters.length; i++) {
+          if (adopters[i] !== "0x0000000000000000000000000000000000000000") {
+            $(".panel-pet")
+              .eq(i)
+              .find("button")
+              .text("Success")
+              .attr("disabled", true);
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   },
 
-  handleAdopt: function(event) {
+  handleAdopt: function (event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    const petId = parseInt($(event.target).data("id"));
 
-    /*
-     * Replace me...
-     */
-  }
+    web3.eth.getAccounts(function (err, accounts) {
+      if (err) {
+        console.log(err);
+      }
 
+      // define adoptionInstance to prevent multiple requests
+      let adoptionInstance;
+      const account = accounts[0];
+
+      App.contracts.Adoption.deployed()
+        .then((instance) => {
+          adoptionInstance = instance;
+          return adoptionInstance.adopt(petId, { from: account });
+        })
+        .then(() => {
+          return App.markAdopted();
+        })
+        .catch((err) => {
+          console.error(err.message);
+        });
+    });
+  },
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
